0e95761ba6e46ad8ddef49c5da2bde6bc00faa05
PRAVEENRANGASAMY <46814112+PRAVEENRANGASAMY@users.noreply.github.com>
2019-03-26 15:55:09 +0530

    Rename 5.c to 5(1).c

---
+++ biggener/set1/5(1).c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(void)
+{
+	int a=34,b=44,c=87;
+	scanf("%d%d%d",&a,&b,&c);
+	if((a>b)&&(a>c))
+	{
+	printf("%d is greater",a);
+	}
+	else if(b>c)
+	{
+	printf("%d is greater",b);
+	}
+	else
+	{
+	printf("%d is greater",c);
+	}
+
+}
